5bf39b6d437eefe4dc4a3aa0af3fc5afda2d722a
tanvi-jain-tcs <jain01tanvi@gmail.com>
2025-01-16 00:55:35 +0530

    initial test changes

--- test/headers.test.js
+++ test/headers.test.js
@@ -1,7 +1,39 @@
 import { html, fixture, expect } from '@open-wc/testing';
-import { stub } from 'sinon';
-import '../src/header/Header.js';
-
+import sinon, { stub} from 'sinon';
+import { Header } from '../src/header/Header.js';
 describe('loan-header', () => {
   // Write test cases inside this block
+  let el, enBtn, nlBtn, localeChangedSpy;
+
+  beforeEach(async () => {
+    el = await fixture('<loan-header></loan-header>');
+    enBtn = el.shadowRoot?.getElementById('en-GB');
+    nlBtn = el.shadowRoot?.getElementById('nl-NL');
+    localeChangedSpy = sinon.spy(el, 'localeChanged');
+  });
+
+  it('check the en-GB button on load', async() => {
+  expect(enBtn.getAttribute('class')).to.contain('bg-btn-color');
+  expect(nlBtn.getAttribute('class')).not.to.contain('bg-btn-color');
+  expect(enBtn.getAttribute('class')).not.to.contain('btn-cursor');
+  expect(nlBtn.getAttribute('class')).to.contain('btn-cursor');
+  })
+
+  it('check the nl-NL button Click', async() => {
+    nlBtn.click()
+    expect(localeChangedSpy.callCount).to.equal(1);
+    expect(enBtn.getAttribute('class')).not.to.contain('bg-btn-color');
+    expect(nlBtn.getAttribute('class')).to.contain('bg-btn-color');
+    expect(enBtn.getAttribute('class')).to.contain('btn-cursor');
+    expect(nlBtn.getAttribute('class')).not.to.contain('btn-cursor');
+    })
+
+    it('check the en-GB button Click', async() => {
+      enBtn.click()
+      expect(localeChangedSpy.callCount).to.equal(1);
+      expect(enBtn.getAttribute('class')).to.contain('bg-btn-color');
+      expect(nlBtn.getAttribute('class')).not.to.contain('bg-btn-color');
+      expect(enBtn.getAttribute('class')).not.to.contain('btn-cursor');
+      expect(nlBtn.getAttribute('class')).to.contain('btn-cursor');
+      })
 });
